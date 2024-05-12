d84d5b55cc8ac736b0be1e3a3c0c69855f69ad88
mohamed teama <MohamedATeama>
2024-05-13 00:38:41 +0300

    update main page javascript

--- E-Commerce/js/script.js
+++ E-Commerce/js/script.js
@@ -173,3 +173,126 @@ cart.addEventListener('click', () => {
         }
     }
 });
+
+function showDetails(id) {
+    window.location = "details.html";
+    localStorage.setItem("selectedId", id);
+}
+
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+let allpage = document.querySelector(".allpage");
+let result = document.querySelector(".searchResult");
+
+searchBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (searchInput.value.trim() === "") {
+        allpage.style.display = "block";
+        result.style.display = "none";
+    } else {
+        search(searchInput.value, allProducts);
+    }
+});
+
+searchInput.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (searchInput.value.trim() === "") {
+        allpage.style.display = "block";
+        result.style.display = "none";
+    } else {
+        search(searchInput.value, allProducts);
+    }
+});
+
+function search(title, arr) {
+    let myarr = arr.filter((item) => item.name.toLowerCase().indexOf(title.toLowerCase()) > -1);
+    searchResult(myarr);
+}
+
+function searchResult(arr) {
+    allpage.style.display = "none";
+    result.style.display = "flex";
+    let html = "";
+    for (const product of arr) {
+        html += `<div class="card text-center p-0" style="width: 400px !important; margin-top: 100px;">
+        <img onclick="showDetails(${product.id})" class="card-img-top p-3" src="${product.img}" alt="Card image" style="height: 300px;">
+        <div class="card-body">
+            <h5 onclick="showDetails(${product.id})" class="card-title">${product.name}</h5>
+            <p class="card-text desc">${product.desc}</p>
+            <h6 class="card-subtitle mb-2 text-muted">${product.price}</h6>
+            <div class="d-flex justify-content-evenly align-items-center mt-3">
+                <button onclick="addToCart(${product.id})" class="btn btn-outline-secondary rounded-4">Add to Cart</button>
+                <span onclick="addToFav(${product.id})"><i class="fa-solid fa-heart fs-4 text-secondary" style="color: ${product.liked == true ? 'red !important' : ''}"></i></span>
+            </div>
+        </div>
+    </div>`;
+    }
+    result.innerHTML = html;
+}
+
+let favItems = localStorage.getItem("favItems") ? JSON.parse(localStorage.getItem("favItems")) : [];
+let favCount = document.getElementById("favCount");
+
+function addToFav(id) {
+    if (!localStorage.getItem("email")) {
+        window.location = "login.html";
+    } else {
+        let product = allProducts.find((item) => item.id == id);
+        product.liked = true;
+
+        if (!favItems.find(e => e.id == product.id)) {
+            favItems.push(product);
+        }
+
+        localStorage.setItem("favItems", JSON.stringify(favItems));
+
+        favCount.innerHTML = favItems.length;
+        favCount.style.display = "block";
+
+        allProducts.map(e => {
+            if (e.id === product.id) {
+                e.liked = true;
+            }
+        });
+
+        if (allpage.style.display == "none") { search(searchInput.value, allProducts); }
+
+        drawPage();
+    }
+}
+
+if (favItems.length > 0) {
+    favItems.map(item => {
+        allProducts.map(e => {
+            if (e.id === item.id) {
+                e.liked = true;
+            }
+        });
+    });
+    drawPage();
+    favCount.innerHTML = favItems.length;
+    favCount.style.display = "block";
+}
+
+let fav = document.getElementById("fav");
+
+fav.addEventListener('click', function () {
+    if (favItems.length > 0) {
+        window.location.href = "favourite.html";
+    }
+});
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(".appleSection", {});
+sr.reveal(".samsungSection", { delay: 100 });
+sr.reveal(".oppoSection", { delay: 100 });
+sr.reveal(".realmeSection", { delay: 100 });
+sr.reveal(".xiaomiSection", { delay: 100 });
+sr.reveal(".huaweiSection", { delay: 100 });
+sr.reveal(".otherSection", { delay: 100 });
